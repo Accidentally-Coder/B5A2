c8d593c427e46a2bad6a2803a79e997a56dc8ad1
Farzana Reefat Raha <reefat.raha2018@gmail.com>
2025-05-27 21:57:17 +0600

    Added sample data

--- query.sql
+++ query.sql
@@ -21,11 +21,107 @@ SELECT * from species;
 -- creating table : sightings
 CREATE table sightings (
     sighting_id SERIAL UNIQUE NOT NULL PRIMARY KEY,
-    ranger_id INTEGER REFERENCES rangers (ranger_id),
     species_id INTEGER REFERENCES species (species_id),
-    sighting_time DATE,
+    ranger_id INTEGER REFERENCES rangers (ranger_id),
     location VARCHAR(50),
+    sighting_time TIMESTAMP,
     notes TEXT
 );
 
 SELECT * from sightings;
+
+-- inserting data to table : rangers
+INSERT INTO
+    rangers (ranger_id, name, region)
+VALUES (
+        1,
+        'Alice Green',
+        'Northern Hills'
+    ),
+    (2, 'Bob White', 'River Delta'),
+    (
+        3,
+        'Carol King',
+        'Mountain Range'
+    );
+
+-- inserting data to table : species
+INSERT INTO
+    species (
+        species_id,
+        common_name,
+        scientific_name,
+        discovery_date,
+        conservation_status
+    )
+VALUES (
+        1,
+        'Snow Leopard',
+        'Panthera uncia',
+        '1775-01-01',
+        'Endangered'
+    ),
+    (
+        2,
+        'Bengal Tiger',
+        'Panthera tigris tigris',
+        '1758-01-01',
+        'Endangered'
+    ),
+    (
+        3,
+        'Red Panda',
+        'Ailurus fulgens',
+        '1825-01-01',
+        'Vulnerable'
+    ),
+    (
+        4,
+        'Asiatic Elephant',
+        'Elephas maximus indicus',
+        '1758-01-01',
+        'Endangered'
+    );
+
+-- inserting data to table : sightings
+INSERT INTO
+    sightings (
+        sighting_id,
+        ranger_id,
+        species_id,
+        location,
+        sighting_time,
+        notes
+    )
+VALUES (
+        1,
+        1,
+        1,
+        'Peak Ridge',
+        '2024-05-10 07:45:00',
+        'Camera trap image captured'
+    ),
+    (
+        2,
+        2,
+        2,
+        'Bankwood Area',
+        '2024-05-12 16:20:00',
+        'Juvenile seen'
+    ),
+    (
+        3,
+        3,
+        3,
+        'Bamboo Grove East',
+        '2024-05-15 09:10:0',
+        'Feeding observed'
+    ),
+    (
+        4,
+        1,
+        2,
+        'Snowfall Pass',
+        '2024-05-18 18:30:00',
+        NULL
+    );
